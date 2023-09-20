5835cc3b58b05fa12e7f6a1787deab13df3993cd
PavelK6896 <79208776896@yandex.ru>
2023-09-20 21:42:28 +0300

    add save method

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from fastapi.responses import FileResponse
 import os
 from random import randint
 import uuid
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, Depends, Form
 from fastapi.responses import RedirectResponse, HTMLResponse
 from typing import Optional
 import urllib
+from dataclasses import dataclass
+from typing import Annotated
+
+from fastapi import FastAPI, Form
 
 from predict import adapt, predict
 
@@ -27,20 +31,29 @@ async def root():
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/save")
+async def save_file(right: Annotated[str, Form()], image: UploadFile = File(...)):
+
+    if not os.path.exists(f"{IMAGEDIR}{right}"):
+        os.makedirs(f"{IMAGEDIR}{right}")
+
+    filename = f"{uuid.uuid4()}.png"
+    right = right + '/'
+    contents = await image.read()
+    d = adapt(contents)
+    d.save(f"{IMAGEDIR}{right}{filename}")
+
+    return {"filename": filename, "right": right}
+
 
 @app.post("/images")
 async def create_upload_file(
         image: UploadFile = File(...)
 ):
-    filename = f"{uuid.uuid4()}.png"
     contents = await image.read()
     d = adapt(contents)
     r = predict(d)
-
-    with open(f"{IMAGEDIR}{filename}", "wb") as f:
-        f.write(contents)
-
-    return {"filename": filename, "r": str(r)}
+    return {"result": str(r)}
 
 
 import uvicorn
